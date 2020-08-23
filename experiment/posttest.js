714b471e936c8d9812a5be852eeaa5fc863b0248
Shubham Gupta <shubham1908gupta@gmail.com>
2020-08-23 22:32:02 +0530

    Update posttest.js

--- experiment/posttest.js
+++ experiment/posttest.js
@@ -90,25 +90,25 @@
 
   const myQuestions = [
     {
-      question: "This is question 1?",  ///// Write the question inside double quotes
+      question: "Which line is responsible for switching of square wave between high and low?",  ///// Write the question inside double quotes
       answers: {
-        a: "Option 1",                  ///// Write the option 1 inside double quotes
-        b: "Option 2",                  ///// Write the option 2 inside double quotes
-        c: "Option 3",                  ///// Write the option 3 inside double quotes
-        d: "Option 4"                   ///// Write the option 4 inside double quotes
+        a: "SJMP squarewave",                  ///// Write the option 1 inside double quotes
+        b: "CPL P1.0",                  ///// Write the option 2 inside double quotes
+        c: "ACALL Delay",                  ///// Write the option 3 inside double quotes
+        d: "None"                   ///// Write the option 4 inside double quotes
       },
-      correctAnswer: "a"                ///// Write the correct option inside double quotes
+      correctAnswer: "b"                ///// Write the correct option inside double quotes
     },
 
     {
-      question: "This is question 2?",  ///// Write the question inside double quotes
+      question: "Which line is responsible for the horizontal lines seen on the graph?",  ///// Write the question inside double quotes
       answers: {
-        a: "Option 1",                  ///// Write the option 1 inside double quotes
-        b: "Option 2",                  ///// Write the option 2 inside double quotes
-        c: "Option 3",                  ///// Write the option 3 inside double quotes
-        d: "Option 4"                   ///// Write the option 4 inside double quotes
+        a: "SJMP squarewave",                  ///// Write the option 1 inside double quotes
+        b: "CPL P1.0",                  ///// Write the option 2 inside double quotes
+        c: "ACALL Delay",                  ///// Write the option 3 inside double quotes
+        d: "None"                   ///// Write the option 4 inside double quotes
       },
-      correctAnswer: "b"                ///// Write the correct option inside double quotes
+      correctAnswer: "c"                ///// Write the correct option inside double quotes
     },                                  ///// To add more questions, copy the section below 
     									                  ///// this line
 
